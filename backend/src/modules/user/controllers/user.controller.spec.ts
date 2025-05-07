4f2340e26c402ecbc99f17459873a6502bf6f1e8
Diogo Pina <diogojpina@gmail.com>
2025-05-07 08:12:43 -0300

    Add user controller unit test

---
+++ backend/src/modules/user/controllers/user.controller.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from '../services/user.service';
+import { User } from '@prisma/client';
+import { CreateUserDto } from '../dtos/create.user.dto';
+
+const user: User = {
+  id: '6814da8c52536908b84bdb69',
+  name: 'Diogo Pina',
+  email: 'diogojpina@gmail.com',
+  password: '',
+};
+
+describe('UserController', () => {
+  let controller: UserController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+    })
+      .useMocker((token) => {
+        if (token === UserService) {
+          return {
+            create: jest.fn().mockResolvedValue(Promise.resolve(user)),
+          };
+        }
+      })
+      .compile();
+
+    controller = module.get<UserController>(UserController);
+  });
+
+  describe('create', () => {
+    it('should create an expense', async () => {
+      const dto = new CreateUserDto();
+      dto.email = user.email;
+      dto.name = user.name;
+      dto.password = '123456';
+
+      const expense = await controller.create(dto);
+      expect(expense).toBe(user);
+    });
+  });
+});
--- backend/src/modules/user/controllers/user.controller.ts
+++ backend/src/modules/user/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { CreateUserDto } from '../dtos/create.user.dto';
 import { User } from '@prisma/client';
-import { IsPublic } from 'src/common';
+import { IsPublic } from '../../../common';
 
 @Controller('user')
 export class UserController {
